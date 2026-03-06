0acc52622e2ade57dfbdf982cc17dded62040c37
endzju <andrebaronstudent@gmail.com>
2026-03-06 22:57:00 +0100

    alfa-beta problem

--- project1/game_run.py
+++ project1/game_run.py
@@ -1,48 +1,88 @@
 from easyAI import AI_Player, Human_Player, Negamax
 from easyAI.games.Hexapawn import Hexapawn
 from hexapawn_prob import HexapawnProb
+import time
+
+def play_game(game_class, ai, num_games, nmoves = 50) -> tuple[int, int]:
+    
+    player1_wins = 0
+    player2_wins = 0
+    start = time.time()
+    for i in range(num_games):
+        game = game_class([AI_Player(ai), AI_Player(ai)], size=(5,5))
+        game.current_player = 1 + i%2
+        game.play(nmoves=nmoves, verbose=False)
+
+        if game.opponent_index == 1:
+            player1_wins += 1
+        elif game.opponent_index == 2:
+            player2_wins += 1
+        else:
+            raise ValueError("Invalid opponent index")
+    end = time.time()
+    print(f"Class: {game_class}")
+    print("Number of games: ", num_games)
+    print(f"Time: {end - start:.4f} s")
+    print("Player 1 wins: ", player1_wins)
+    print("Player 2 wins: ", player2_wins)
+
 
 scoring = lambda game: -100 if game.lose() else 0
-ai = Negamax(10, scoring)
 
-player1 = AI_Player(ai)
-player2 = AI_Player(ai)
+# num_games = 10
+# depth = 10
+# ai = Negamax(depth, scoring)
+# play_game(game_class=Hexapawn, ai=ai, num_games=num_games)
+# print(f"Depth: {depth}")
+# print("without alpha-beta pruning")
+# print()
+# play_game(game_class=HexapawnProb, ai=ai, num_games=num_games)
+# print(f"Depth: {depth}")
+# print("without alpha-beta pruning")
+# print()
 
-num_games = 10
-player1_wins = 0
-player2_wins = 0
+# depth = 5
+# ai = Negamax(depth, scoring)
+# play_game(game_class=Hexapawn, ai=ai, num_games=num_games)
+# print(f"Depth: {depth}")
+# print("without alpha-beta pruning")
+# print()
+# play_game(game_class=HexapawnProb, ai=ai, num_games=num_games)
+# print(f"Depth: {depth}")
+# print("without alpha-beta pruning")
+# print()
 
+# depth = 10
+# ai = Negamax(depth, scoring, win_score=101)
+# play_game(game_class=Hexapawn, ai=ai, num_games=num_games)
+# print(f"Depth: {depth}")
+# print("with alpha-beta pruning")
+# print()
+# play_game(game_class=HexapawnProb, ai=ai, num_games=num_games)
+# print(f"Depth: {depth}")
+# print("with alpha-beta pruning")
+# print()
 
-for i in range(num_games):
-    game = Hexapawn([player1, player2])
-    game.current_player = 1 + i%2
-    game.play(nmoves=1, verbose=False)
+# depth = 5
+# ai = Negamax(depth, scoring, win_score=101)
+# play_game(game_class=Hexapawn, ai=ai, num_games=num_games)
+# print(f"Depth: {depth}")
+# print("with alpha-beta pruning")
+# print()
+# play_game(game_class=HexapawnProb, ai=ai, num_games=num_games)
+# print(f"Depth: {depth}")
+# print("with alpha-beta pruning")
+# print()
 
-    if game.opponent_index == 1:
-        player1_wins += 1
-    elif game.opponent_index == 2:
-        player2_wins += 1
-    else:
-        raise ValueError("Invalid opponent index")
-
-print("Default Hexspawn, number of games: ", num_games)
-print("Player 1 wins: ", player1_wins)
-print("Player 2 wins: ", player2_wins)
-print("Draws: ", num_games - player1_wins - player2_wins)
-
-for i in range(num_games):
-    game = HexapawnProb([player1, player2])
-    game.current_player = 1 + i%2
-    game.play(nmoves=1, verbose=False)
-
-    if game.opponent_index == 1:
-        player1_wins += 1
-    elif game.opponent_index == 2:
-        player2_wins += 1
-    else:
-        raise ValueError("Invalid opponent index")
-
-print("HexapawnProb, number of games: ", num_games)
-print("Player 1 wins: ", player1_wins)
-print("Player 2 wins: ", player2_wins)
-print("Draws: ", num_games - player1_wins - player2_wins)
+num_games = 1
+depth = 9
+ai = Negamax(depth, scoring)
+play_game(game_class=Hexapawn, ai=ai, num_games=num_games)
+print(f"Depth: {depth}")
+print("without alpha-beta pruning")
+print()
+ai = Negamax(depth, scoring, win_score=101)
+play_game(game_class=Hexapawn, ai=ai, num_games=num_games)
+print(f"Depth: {depth}")
+print("with alpha-beta pruning")
+print()
